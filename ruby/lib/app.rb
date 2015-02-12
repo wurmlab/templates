59471fa2429f6d75b1766c0a0ec00531e2372c77
Anurag Priyam <anurag08priyam@gmail.com>
2015-02-12 18:00:53 +0530

    Remove TODOs which were taken care of by rbgen script.
    And slightly change message printed by App#run.
    
    Signed-off-by: Anurag Priyam <anurag08priyam@gmail.com>

--- ruby/lib/app.rb
+++ ruby/lib/app.rb
@@ -4,17 +4,12 @@
 #   here.
 require 'app/componentA'
 
-# TODO:
-#   1. Rename App to the name of your app in camelcase - like SequenceServer.
-#   2. Document what it does.
 class App
-
   # NOTE:
   #   Methods that should be called as 'App.function_name', or class methods
   #   should be defined here.
   class << self
-
-    # Return version number from 'gemspec' file.
+    # Returns version number from 'gemspec' file.
     def version
       gemspec_file = File.join(File.dirname(__FILE__), '..', 'app.gemspec')
       gemspec = eval File.read gemspec_file
@@ -34,6 +29,6 @@ class App
   # Instance methods.
 
   def run
-    puts "Will run analysis on: #{files} with the following options: #{options}"
+    puts "Will process: #{files}, with the following options: #{options}."
   end
 end
--- ruby/lib/app/componentA.rb
+++ ruby/lib/app/componentA.rb
@@ -1,7 +1,5 @@
 class App
-
   # A component of the app that does one thing in paritular.
   class ComponentA
-
   end
 end
